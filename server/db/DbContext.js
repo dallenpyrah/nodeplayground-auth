609431646265d582d37c5bd9223703ea596a2acd
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-04 08:21:20 -0600

    Monkey model and schema set up

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -3,6 +3,7 @@ import ValueSchema from '../models/Value'
 import AccountSchema from '../models/Account'
 import CarSchema from '../models/Car'
 import JungleSchema from '../models/Jungle'
+import MonkeySchema from '../models/Monkey'
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
@@ -10,6 +11,8 @@ class DbContext {
   Car = mongoose.model('Car', CarSchema);
 
   Jungle = mongoose.model('Jungle', JungleSchema)
+
+  Monkey = mongoose.model('Monkey', MonkeySchema)
 }
 
 export const dbContext = new DbContext()
---
+++ server/models/Monkey.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+const { Schema } = require('mongoose')
+const ObjectId = mongoose.SchemaTypes.ObjectId
+
+const Monkey = new Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    favoriteFood: { type: String },
+    canFight: { type: Boolean, required: true },
+    children: { type: Number },
+    jungleId: { type: ObjectId, ref: 'Jungle', required: true },
+    creatorId: { type: String, ref: 'Account', required: true }
+  }, { timestamps: true, toJSON: { virtuals: true } })
+
+export default Monkey
